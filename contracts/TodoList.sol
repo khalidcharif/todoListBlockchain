5024bbe6c4a8ab88700f7789f6939fa28cbdc6de
khalid charif <khalidcharif@gmail.com>
2021-05-05 23:16:29 +0000

    second commit test & update contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -15,9 +15,16 @@ contract TodoList {
         createTask("Check out dappuniversity.com");
       }
 
-  function createTask(string memory _content) public {
+   event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 
 }
